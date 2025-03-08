3557191481134427843f2b98da5055d16997fcec
CR3Y745 <creytas@gmail.com>
2025-03-08 18:39:22 +0100

    feat: create employee page functions structure

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
+  changePage(direction:string) {
+    console.log('Changing page to ' + direction);
+  }
 
+  view() {
+    console.log('Viewing employee details');
+  }
+
+  edit() {
+    console.log('Editing employee details');
+  }
+
+  delete() {
+    console.log('Deleting employee details');
+  }
+
+  close() {
+    console.log('Closing the employee registration page');
+  }
+
+  add() {
+    console.log('Adding a new employee');
+  }
+
+  export() {
+    console.log('Exporting employee data to CSV');
+  }
 }
