171a3d87718c3a1778b3cf998909e1f31df451f3
CR3Y745 <creytas@gmail.com>
2025-03-09 15:23:55 +0100

    feat: display form on add new click

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MasterService } from '../../services/master.service';
 import { Employee, IApiResponse, IDepartment } from '../../model/Employee';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
